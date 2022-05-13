e9e90b67da63cc075fc1a47ee6376c409a268e35
Martin Lazarevski <martin.lazarevski@generadevelopment.com>
2022-05-13 12:21:53 +0200

    add elementor and custome theme

---
+++ wp-content/themes/pabau_assignment/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php wp_head();?>
+</head>
+<body <?php body_class();?>>
+<nav class="navbar navbar-light bg-light">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+      <img src="<?php echo esc_url( get_template_directory_uri() . '/img/logo.png' ); ?>" alt="" width="30" class="d-inline-block align-text-top">
+      Medoc
+    </a>
+  </div>
+</nav>
--- wp-content/themes/pabau_assignment/index.php
+++ wp-content/themes/pabau_assignment/index.php
@@ -1,3 +1,2 @@
 <?php
-
-?>
+//Index.php
