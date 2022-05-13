7efaa126b240dc105ee23cb5a5bac7f74f18a3e9
Martin Lazarevski <martin.lazarevski@generadevelopment.com>
2022-05-13 20:34:52 +0200

    add another form inputs

--- wp-content/themes/pabau_assignment/footer.php
+++ wp-content/themes/pabau_assignment/footer.php
@@ -5,38 +5,17 @@
 
   <!-- Section: Links  -->
   <section class="">
-    <div class="container text-center text-md-start mt-5">
+    <div class="container text-center text-md-start">
       <!-- Grid row -->
-      <div class="row mt-3">
+      <div class="row">
         <!-- Grid column -->
         <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
           <!-- Content -->
-          <h6 class="text-uppercase fw-bold mb-4">
+          <h6 class="text-uppercase fw-bold mb-4 pt-100">
            Medoc
           </h6>
           <p>
             Worlds best medical equipment
-          </p>
-        </div>
-        <!-- Grid column -->
-
-        <!-- Grid column -->
-        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-          <!-- Links -->
-          <h6 class="text-uppercase fw-bold mb-4">
-            Products
-          </h6>
-          <p>
-            <a href="#!" class="text-reset">Angular</a>
-          </p>
-          <p>
-            <a href="#!" class="text-reset">React</a>
-          </p>
-          <p>
-            <a href="#!" class="text-reset">Vue</a>
-          </p>
-          <p>
-            <a href="#!" class="text-reset">Laravel</a>
           </p>
         </div>
         <!-- Grid column -->
@@ -84,7 +63,7 @@
   <!-- Section: Links  -->
 
   <!-- Copyright -->
-  <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+  <div class="text-center p-4" style="background-color: whitesmoke">
     © 2022 Copyright. All rights reserved.
   </div>
   <!-- Copyright -->
--- wp-content/themes/pabau_assignment/js/formSubmit.js
+++ wp-content/themes/pabau_assignment/js/formSubmit.js
@@ -1,5 +1,5 @@
 jQuery("#forminator-module-59").on("submit", function(){
-    var redirect_link = "https://www.pabau.com"
+    var redirect_link = "https://www.pabau.com";
     jQuery.ajax({
         method:"POST",
         url: "https://recruitment.pabau.me/OAuth2/leads/lead-curl.php",
@@ -8,6 +8,12 @@ jQuery("#forminator-module-59").on("submit", function(){
             "redirect_link": redirect_link,
             "Fname":jQuery("#forminator-field-first-name-1").val(),
             "Lname":jQuery("#forminator-field-last-name-1").val(),
+            "mobile":jQuery("#forminator-field-text-3").val(),
+            "email":jQuery("#forminator-field-text-4").val(),
+            "lead_source":jQuery("#select-1").val(),
+            "dob":jQuery("#forminator-field-date-1-picker-627e965dd8e75").val(),
+            "country":jQuery("#forminator-field-text-1").val(),
+            "city":jQuery("#forminator-field-text-2").val(),
         },
         success : function( response ) {
             window.location.replace(redirect_link);
